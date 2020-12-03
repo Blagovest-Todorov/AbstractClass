0b44a71091f5fcb40ac94bfbbb6c0638c46d8feb
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-12-03 10:31:52 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _25.AbstractClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // Abstract class  can never be instantiated, no instance made ! 
+           // and abstract class is marked with keyword abstract ! 
+          // Abstract Class should have a minimum of one abstrasct method!  
+        }
+    }
+}
